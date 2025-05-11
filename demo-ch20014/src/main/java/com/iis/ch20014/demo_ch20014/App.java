540d20c52d20c9b100aa59b45d4dbeda9c31b063
Liliana2603 <CH20014@ues.edu.sv>
2025-05-11 01:30:44 -0600

    commit javaEclipse

---
+++ demo-ch20014/src/main/java/com/iis/ch20014/demo_ch20014/App.java
@@ -0,0 +1,17 @@
+package com.iis.ch20014.demo_ch20014;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public String Hello() {
+		return " Hello world ";
+	}
+    public static void main( String[] args )
+    {
+       App hw = new App();
+       System.out.println( hw.Hello() );
+    }
+}
